43665d9b7425272ac77a60c589e3456095b258e2
Brian Zoetewey <brian.zoetewey@cru.org>
2020-04-27 14:42:59 -0400

    Implement current mapping to tealium.

--- models/tealium-event.js
+++ models/tealium-event.js
@@ -6,43 +6,69 @@ import {
   isNil,
   map,
   mapValues,
-  omitBy
+  omitBy,
+  transform
 } from 'lodash'
 import Context from './context'
 
 const DataLayerMapping = {
-  app_name: 'property_name',
+  property_name: 'app_name',
   platform: 'platform',
-  event_name: 'event',
+  event: 'event_name',
   event_id: 'event_id',
-  client_ip: 'user_ipaddress',
-  tealium_visitor_id: 'domain_userid',
-  site_region: 'geo_region',
-  customer_country: 'geo_country',
-  customer_city: 'geo_city',
-  customer_zip: 'geo_zipcode',
-  'Current URL': 'page_url',
-  'Page Title': 'page_title',
-  'Referring URL': 'page_referrer',
-  Pathname: 'page_urlpath',
-  event_target: 'se_action',
-  event_name: 'se_label',
-  order_id: 'tr_orderid',
-  order_total: 'tr_total',
-  order_tax: 'tr_tax',
-  order_shipping: 'tr_shipping',
-  customer_city: 'tr_city',
-  customer_state: 'tr_state',
-  product_id: 'ti_sku',
-  product_category: 'ti_category',
-  product_unit_price: 'ti_price',
-  product_quantity: 'ti_quantity',
-  'user agent': 'useragent',
+  user_ipaddress: 'Client IP',
+  // domain_userid: 'tealium_visitor_id', // tealium_visitor_id is handled in the identity section
+  network_userid: 'tealium_thirdparty_visitor_id',
+  geo_country: 'geo_country',
+  geo_region: 'geo_region',
+  geo_city: 'geo_city',
+  geo_zipcode: 'geo_zipcode',
+  geo_latitude: 'geo_latitude',
+  geo_longitude: 'geo_longitude',
+  geo_region_name: 'geo_region_name',
+  page_url: 'Current URL',
+  page_title: 'Page Title',
+  page_referrer: 'Referring URL',
+  page_urlpath: 'Pathname',
+  mkt_medium: 'mkt_medium',
+  mkt_source: 'mkt_source',
+  mkt_term: 'mkt_term',
+  mkt_content: 'mkt_content',
+  mkt_campaign: 'mkt_campaign',
+  se_category: 'se_category',
+  se_action: 'se_action',
+  se_label: 'se_label',
+  se_property: 'se_property',
+  se_value: 'se_value',
+  tr_orderid: 'order_id',
+  tr_total: 'order_total',
+  tr_tax: 'order_tax',
+  tr_shipping: 'order_shipping',
+  tr_city: 'customer_city',
+  tr_state: 'customer_state',
+  tr_country: 'tr_country',
+  ti_sku: 'product_id',
+  ti_name: 'product_name',
+  ti_category: 'product_category',
+  ti_price: 'product_unit_price',
+  ti_quantity: 'product_quantity',
+  useragent: 'user agent',
+  br_name: 'br_name',
+  br_family: 'br_family',
+  br_version: 'br_version',
+  br_type: 'br_type',
+  br_lang: 'br_lang',
   os_name: 'os_name',
-  model_name: 'os_family',
-  device: 'dvce_type'
+  os_family: 'os_family',
+  os_manufacturer: 'os_manufacturer',
+  os_timezone: 'os_timezone',
+  dvce_type: 'device',
+  dvce_ismobile: 'dvce_ismobile',
+  geo_timezone: 'geo_timezone',
+  domain_sessionid: 'tealium_session_id',
+  true_tstamp: 'tealium_timestamp_epoch'
 }
-const uuidFields = ['network_userid', 'sso_guid', 'gr_master_person_id']
+const uuidFields = ['sso_guid', 'gr_master_person_id']
 const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
 
 class TealiumEvent {
@@ -55,9 +81,9 @@ class TealiumEvent {
     return omitBy({
       ...this.standardParameters,
       ...this.identityParameters,
-      ...mapValues(DataLayerMapping, (v, k) => {
-        return this.event[v]
-      })
+      ...transform(DataLayerMapping, (result, value, key) => {
+        result[value] = this.event.data[key]
+      }, {}),
     }, isNil)
   }
 
@@ -82,13 +108,10 @@ class TealiumEvent {
         })
       }
     }
-  }
+    // Set tealium_visitor_id to the domain_userid or device_idfa, whichever is first present.
+    identityParams.tealium_visitor_id = head(compact([this.event.data.domain_userid, identityParams.device_idfa]))
 
-  fieldValue (value, field) {
-    if (includes(uuidFields, field)) {
-      return uuidPattern.test(value) ? value : /* istanbul ignore next */ undefined
-    }
-    return value
+    return identityParams
   }
 
   get standardParameters () {
@@ -97,6 +120,13 @@ class TealiumEvent {
       tealium_profile: 'main',
       tealium_datasource: 'snowplow'
     }
+  }
+
+  fieldValue (value, field) {
+    if (includes(uuidFields, field)) {
+      return uuidPattern.test(value) ? value : /* istanbul ignore next */ undefined
+    }
+    return value
   }
 }
 
