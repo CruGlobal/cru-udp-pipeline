434c17189e2c2edbffb0c2e21329493f527b1f82
Brian Zoetewey <brian.zoetewey@cru.org>
2020-07-16 10:34:19 -0400

    data not on this yet

--- models/event.js
+++ models/event.js
@@ -27,8 +27,8 @@ class Event {
     this.event_id = data.event_id
     this.type = data.event_name
 
-    if (process.env.LOG_LEVEL === 'debug' && this.data.event_type === 'struct') {
-      console.log(this.event_id, JSON.stringify(this.data))
+    if (process.env.LOG_LEVEL === 'debug' && data.event_type === 'struct') {
+      console.log(this.event_id, JSON.stringify(data))
     }
 
     // Will throw errors if event is not a valid DerivedEvent
